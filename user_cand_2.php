e62e8d522a37eb6ac3b2578f352fbe181f15527b
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-09-13 01:37:17 +0200

    Update user_cand_2.php

--- user_cand_2.php
+++ user_cand_2.php
@@ -26,8 +26,8 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
  
 
  <!-- Bootstrap CSS -->
- <link rel="stylesheet" href="css/bootstrap.min.css">
- <link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="https://online-voting-system.multireligionvalsystem.eu.org/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://online-voting-system.multireligionvalsystem.eu.org/css/style.css">
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
  <title></title>
 </head>
@@ -35,34 +35,34 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div class="container-fluid" id="cont-3">
         <header id="nav-bar">
           <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-            <a class="navbar-brand" href=index.html  style="color: white; font-weight: 600; margin-top: 15px;">GO VOTE</a>
+            <a class="navbar-brand" href=https://online-voting-system.multireligionvalsystem.eu.org/index.html  style="color: white; font-weight: 600; margin-top: 15px;">GO VOTE</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon" style="color: white;"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ml-auto animate__animated animate__bounceInDown" style="padding-right: 50px;">
                 <li class="nav-item" >
-                  <a class="nav-link" href="index.html" style="color:white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Home</a>
+                  <a class="nav-link" href="https://online-voting-system.multireligionvalsystem.eu.org/index.html" style="color:white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Home</a>
                 </li>
                 <li class="nav-item" >
-                  <a class="nav-link" href="admin_dashboard.php" style="color:white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Dashboard</a>
+                  <a class="nav-link" href="https://online-voting-system.multireligionvalsystem.eu.org/admin_dashboard.php" style="color:white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Dashboard</a>
                 </li>
                 <li class="nav-item" >
-                  <a class="nav-link" href="user_suggestion.php"  style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Suggestions</a>
+                  <a class="nav-link" href="https://online-voting-system.multireligionvalsystem.eu.org/user_suggestion.php"  style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Suggestions</a>
                 </li>
                 <li class="nav-item" >
-                  <a class="nav-link" href="show_contact.php"  style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Contact</a>
+                  <a class="nav-link" href="https://online-voting-system.multireligionvalsystem.eu.org/show_contact.php"  style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Contact</a>
                 </li>
               
                 <li class="nav-item">
-                  <a class="nav-link" href="user_details_year.php" style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Users</a>
+                  <a class="nav-link" href="https://online-voting-system.multireligionvalsystem.eu.org/user_details_year.php" style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Users</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="user_cand_year.php" style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Candidates</a>
+                  <a class="nav-link" href="https://online-voting-system.multireligionvalsystem.eu.org/user_cand_year.php" style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Candidates</a>
                 </li>
               
                 <li class="nav-item" >
-                  <a class="nav-link" href="logout.php"  style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Logout</a>
+                  <a class="nav-link" href="https://online-voting-system.multireligionvalsystem.eu.org/logout.php"  style="color: white; font-weight: 600; text-align: center; font-size: 18px; margin-top: 20px;  text-transform: capitalize; padding: 20px;">Logout</a>
                 </li>
               
               </ul>
@@ -124,12 +124,12 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			echo "</td>
 			<td>";
 			if($row['approve_status']==2){
-				echo '<a href="change_status_2nd.php?id='.$cid.'&status=1" class="btn btn-success">Approve</a>';
+				echo '<a href="https://online-voting-system.multireligionvalsystem.eu.org/change_status_2nd.php?id='.$cid.'&status=1" class="btn btn-success">Approve</a>';
 			}else if($row['approve_status']==1){
-				echo '<a href="change_status_2nd.php?id='.$cid.'&status=2" class="btn btn-danger">Reject</a>';
+				echo '<a href="https://online-voting-system.multireligionvalsystem.eu.org/change_status_2nd.php?id='.$cid.'&status=2" class="btn btn-danger">Reject</a>';
 			}else if($row['approve_status']==0){
-				echo '<a href="change_status_2nd.php?id='.$cid.'&status=1" class="btn btn-success">Approve</a>';
-				echo '<a href="change_status_2nd.php?id='.$cid.'&status=2" class="btn btn-danger">Reject</a>';
+				echo '<a href="https://online-voting-system.multireligionvalsystem.eu.org/change_status_2nd.php?id='.$cid.'&status=1" class="btn btn-success">Approve</a>';
+				echo '<a href="https://online-voting-system.multireligionvalsystem.eu.org/change_status_2nd.php?id='.$cid.'&status=2" class="btn btn-danger">Reject</a>';
 			}
 
 			echo "</td>
@@ -147,9 +147,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
       
-        <script src="js/jquery-3.2.1.slim.min.js"></script>
-        <script src="js/popper.min.js"></script>    
-        <script src="js/bootstrap.min.js"></script>  
+        <script src="https://online-voting-system.multireligionvalsystem.eu.org/js/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://online-voting-system.multireligionvalsystem.eu.org/js/popper.min.js"></script>    
+        <script src="https://online-voting-system.multireligionvalsystem.eu.org/js/bootstrap.min.js"></script>  
         
 </body>
 </html>
